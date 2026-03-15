c3fd9f359be1f6a54bbaae10448c2ca3ffe80914
osmitary <osmitary1289@gmail.com>
2026-03-15 21:50:31 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -183,29 +183,43 @@ const now = Date.now();
 if(now - lastTurn < 1500) return;
 
 
-// 右向き
+// 右向き（前ページ）
 
 if(x > 0.65){
 
-status.innerText = "RIGHT → NEXT PAGE";
+if(pageNum > 1){
 
-nextPage();
-
-lastTurn = now;
-
-}
-
-// 左向き
-
-if(x < 0.35){
-
-status.innerText = "LEFT → PREVIOUS PAGE";
+status.innerText = "RIGHT → PREVIOUS PAGE";
 
 prevPage();
 
+}else{
+
+status.innerText = "FIRST PAGE";
+
+}
+
 lastTurn = now;
 
 }
+
+// 左向き（次ページ）
+
+if(x < 0.35){
+
+if(pageNum < pdfDoc.numPages){
+
+status.innerText = "LEFT → NEXT PAGE";
+
+nextPage();
+
+}else{
+
+status.innerText = "LAST PAGE";
+
+}
+
+lastTurn = now;
 
 }
 
